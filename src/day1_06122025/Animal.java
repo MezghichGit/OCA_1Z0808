de5e052a4f742376b164577747b7a2eb061e8005
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-12-06 10:22:54 +0100

    chapitre 1 OCA

---
+++ src/day1_06122025/Animal.java
@@ -0,0 +1,20 @@
+package day1_06122025;
+
+public class Animal {
+	
+	// Les attributs d'instance
+	
+	//Les attributs de classe
+	
+	//Les constructeurs
+	
+	
+	// Les blocs d'initialisation d'instance
+	
+	// Les blocs d'initialisation de classe
+	
+	// Les méthodes d'instance
+	
+	// Les méthodes de classe
+
+}
---
+++ src/day1_06122025/Main.java
@@ -0,0 +1,21 @@
+package day1_06122025;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int tab[]= {10,14, 20};  //tableau de type primitif
+		String names [] = {"oca","ocp"}; // tableau de type reference
+		/*
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		 */
+	}
+
+}
