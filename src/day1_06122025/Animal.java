20448df6dd9233c1ba9c6a468aed72054dafd22c
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-12-06 10:42:36 +0100

    update class member

--- src/day1_06122025/Animal.java
+++ src/day1_06122025/Animal.java
@@ -3,18 +3,47 @@ package day1_06122025;
 public class Animal {
 	
 	// Les attributs d'instance
+	String espece;
+	int age;
 	
 	//Les attributs de classe
+	static int  MAX_AGE = 200;
 	
 	//Les constructeurs
-	
-	
-	// Les blocs d'initialisation d'instance
-	
-	// Les blocs d'initialisation de classe
-	
+	public Animal() {
+		System.out.println("Constructeur par défaut Animal");
+	}
+	public Animal(String espece, int age) {
+		System.out.println("Constructeur avec paramètres Animal");
+		this.espece = espece;
+		this.age = age;
+	}
+
 	// Les méthodes d'instance
+	public String getEspece() {
+		return espece;
+	}
+	public void setEspece(String espece) {
+		this.espece = espece;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
 	// Les méthodes de classe
-
+	public static int getMAX_AGE() {
+		return MAX_AGE;
+	}
+	public static void setMAX_AGE(int mAX_AGE) {
+		MAX_AGE = mAX_AGE;
+	}
+	
+	
+	
+	
+	// Les blocs d'initialisation de classe
+	// Les blocs d'initialisation d'instance
 }
---
+++ src/day1_06122025/TestAnimal.java
@@ -0,0 +1,11 @@
+package day1_06122025;
+
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		Animal a1 = new Animal();
+		Animal a2 = new Animal("Chat",2);
+
+	}
+
+}
